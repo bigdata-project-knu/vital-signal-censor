ca1201c8690bfae6db04205e890c772f471c2521
jinuklee <150532431+jinuk0211@users.noreply.github.com>
2024-03-18 12:52:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from PIL import Image
 from engine import vit, isolationforest
+from engine implementation import get_prediction
 
 app = Flask(__name__)
 
@@ -9,9 +10,24 @@ app = Flask(__name__)
 allowed_data_extension  = {'png','jpeg','jpg'} #edf,rml 변환
 def is_allowed_file(filename):
     return '.' in filename and filename.rsplit('.',1)[1].lower() in allowed_data_extension
-@app.route('/predict',methods =['GET'])
-def prediction():
-
+@app.route('/predict',methods =['POST','GET'])
+    def prediction():
+        if request.method == 'POST':
+            file = request.files.get('file')
+            if file is None or file.filename == '':
+                return jsonify({'error': '파일 없음'})
+            if not is_allowed_file(file.filename):
+                return jsonify({'error': '지원하지 않는 파일형식'})
+            try:
+                img_bytes = file.read() #jpeg, png파일을 바이트로
+                img_tensor = transform_image(img_bytes) # 이미지를 tensor형태로 바꿈
+                prediction = get_prediction(img_tensor) #정상 0, 이상 1
+                data = {'prediction':prediction.item()}
+                return jsonify(data)
+            except e:
+                return jsonify({'error':'ai 파이프라인 오류'})
+        return jsonify({'결과':'수행 종료'})
+                
 
 # export FLASK_APP = main.py
 # export FLASK_DEBUG= 1
