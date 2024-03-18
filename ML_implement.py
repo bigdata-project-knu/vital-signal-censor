e100738466682b268115f53fe0d3a02425e94e0b
jinuklee <150532431+jinuk0211@users.noreply.github.com>
2024-03-18 17:36:48 +0900

    Update ML_implement.py

--- ML_implement.py
+++ ML_implement.py
@@ -1,8 +1,11 @@
 from sklearn.ensemble import IsolationForest
 import pickle
-
+#훈련된 모델을 따로 불러와서 api 에 import
 model = IsolationForest(random_state=0)
 
 with open("isolation_forest.pkl", "rb") as f:
     model = pickle.load(f)
-#isolation forest img-> embedding으로 바꿔줘야함
+#isolation forest img-> embedding으로 바꿔줘야함-------------------------
+
+def get_prediction():
+    pass
