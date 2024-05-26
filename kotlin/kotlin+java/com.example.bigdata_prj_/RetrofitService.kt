47f492198897f93218d0bdcacd08e8c867c9761e
Widadada <106460325+Widadada@users.noreply.github.com>
2024-05-27 01:56:04 +0900

    Update RetrofitService.kt

--- kotlin/kotlin+java/com.example.bigdata_prj_/RetrofitService.kt
+++ kotlin/kotlin+java/com.example.bigdata_prj_/RetrofitService.kt
@@ -1,16 +1,36 @@
 package com.example.bigdata_prj
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RetrofitService {
-    @GET("posts/1")
-    suspend fun getPost1() : POST
+    @FormUrlEncoded
+    @POST("/login")
+    fun requestLogin(
+        @Body jsonParams : UserModel,
+    ) : Call<LoginDTO>
 
-    @GET("posts/{number}")
-    suspend fun getPostNumber(
-        @Path("number") number : Int
-    ) : POST
+    companion object{
+        private const val BASE_URL = "" //백엔드 서버
+        val gson : Gson = GsonBuilder().setLenient().create()
+
+        fun create(): RetrofitService{
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(RetrofitService::class.java)
+        }
+
+    }
 }
