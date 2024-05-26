8c84c4640a2d2c0ef615031a1fcdbbd21fc67202
Widadada <106460325+Widadada@users.noreply.github.com>
2024-05-27 01:56:52 +0900

    Update and rename Post.kt to LoginDTO.kt

---
+++ kotlin/kotlin+java/com.example.bigdata_prj_/LoginDTO.kt
@@ -0,0 +1,7 @@
+package com.example.bigdata_prj
+
+data class LoginDTO(
+    val code: String, //성공or실패
+    val msg: String,
+    //val userInfo: UserInfo
+)
--- kotlin/kotlin+java/com.example.bigdata_prj_/Post.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bigdata_prj
-
-data class Post(
-    var code : String,
-    var msg : String
-)
