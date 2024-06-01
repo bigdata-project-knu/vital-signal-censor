82a649a8166be99998c29e2021b72d748fa257a5
jinuklee <150532431+jinuk0211@users.noreply.github.com>
2024-06-01 19:54:42 +0900

    uploadfile.py 업데이트

--- api/api_audio_nosql.py/uploadfile.py
+++ api/api_audio_nosql.py/uploadfile.py
@@ -14,6 +14,23 @@ def upload_file(file: UploadFile):
         data = json.loads(file.file.read())
     return {"content":data ,"filename":file.filename}
 
+@app.post("/file/uploadndownload")
+def upload_n_downloadfile(file: UploadFile):
+    """Return a YAML FIle for the uploaded JSON File"""
+    if file.content_type != "application/json":
+        raise HTTPException(400,detail="Invalid document type")
+    else:
+        json_data = json.loads(file.file.read())
+        new_filename = "{}_{}.yaml".format(os.path.splitext(file.filename)[0],timestr)
+        # Write the data to a file
+        # Store the saved file
+        SAVE_FILE_PATH = os.path.join(UPLOAD_DIR,new_filename)
+        with open(SAVE_FILE_PATH,"w") as f:
+            yaml.dump(json_data,f)
+
+        # Return as a download
+        return FileResponse(path=SAVE_FILE_PATH,media_type="application/octet-stream",filename=new_filename)
+
 
 if __name__ == '__main__':
     uvicorn.run(app,host="127.0.0.1",port=8000)
