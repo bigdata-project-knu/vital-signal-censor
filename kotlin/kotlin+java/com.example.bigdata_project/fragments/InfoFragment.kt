8d563a3fbd6e108bedff6c516b68df8d0bdbdc97
Widadada <106460325+Widadada@users.noreply.github.com>
2024-06-10 09:16:05 +0900

    Update InfoFragment.kt

--- kotlin/kotlin+java/com.example.bigdata_project/fragments/InfoFragment.kt
+++ kotlin/kotlin+java/com.example.bigdata_project/fragments/InfoFragment.kt
@@ -1,24 +1,27 @@
-package com.example.bigdata_project.fragments
+package com.example.bigdata_prj.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.bigdata_project.databinding.FragmentDataBinding
-import com.example.bigdata_project.databinding.FragmentHomeBinding
+import com.example.bigdata_prj.databinding.FragmentInfoBinding
 
 class InfoFragment : Fragment() {
-
-    private var mBinding : FragmentDataBinding? = null
+    private var mBinding : FragmentInfoBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentDataBinding.inflate(inflater, container, false)
+        val binding = FragmentInfoBinding.inflate(inflater, container, false)
         mBinding = binding
+
+        mBinding?.infoFragmentTitle?.text = "OOO님 안녕하세요"
+        mBinding?.infoFragmentText?.text = "사용자 정보\n\n문의하기\n\n앱 정보"
+
+
         return mBinding?.root
     }
 
