c6e58fe7ce41c1fe752c40a69cf8cb10692b6cca
Widadada <106460325+Widadada@users.noreply.github.com>
2024-06-15 21:45:24 +0900

    Update AppMain.kt

--- kotlin/bigdata_prj/app/kotlin+java/com.example.bigdata_prj/AppMain.kt
+++ kotlin/bigdata_prj/app/kotlin+java/com.example.bigdata_prj/AppMain.kt
@@ -6,9 +6,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.bigdata_prj.databinding.ActivityAppMainBinding
+import com.example.bigdata_prj.fragments.DataFragment
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,53 +24,69 @@ class AppMain : AppCompatActivity() {
 
         val retrofitservice = RetrofitService.create()
 
-        retrofitservice.censor("censor").enqueue(object : Callback<List<Double>>{
-            override fun onResponse(call: Call<List<Double>>, response: Response<List<Double>>) {
-                if(response.isSuccessful.not()){
-                    Log.d("Fail", "NoCheck")
-                    return
+        val currentDataCall = retrofitservice.censor("censor")
+        val predictDataCall = retrofitservice.predict("predict")
+
+        var currentDataArray: DoubleArray? = null
+        var predictDataArray: DoubleArray? = null
+
+        currentDataCall.enqueue(object : Callback<Array<Double>>{
+            override fun onResponse(call: Call<Array<Double>>, response: Response<Array<Double>>) {
+                if(response.isSuccessful){
+                    val currentData = response.body()
+                    currentDataArray = currentData?.toDoubleArray()
+                    currentDataArray?.forEach { value ->
+                        Log.d("censor", value.toString())
+                    }
+                    if(currentDataArray != null && predictDataArray != null){
+                        navigateToDataFragment(currentDataArray!!, predictDataArray!!)
+                    }
                 }
-                val currentData = response.body()
-                currentData?.forEach {value ->
-                    Log.d("censor", value.toString())
+                else{
+                    Log.d("Fail", "currentDataCall")
                 }
             }
-
-            override fun onFailure(call: Call<List<Double>>, t: Throwable) {
-                Log.d("Fail", "Fail: ${t.message}")
-            }
-        })
-
-        retrofitservice.predict("censor").enqueue(object : Callback<List<Double>>{
-            override fun onResponse(call: Call<List<Double>>, response: Response<List<Double>>) {
-                if(response.isSuccessful.not()){
-                    Log.d("Fail", "NoCheck")
-                    return
-                }
-                val predictData = response.body()
-                predictData?.forEach {value ->
-                    Log.d("predict", value.toString())
-                }
-            }
-
-            override fun onFailure(call: Call<List<Double>>, t: Throwable) {
-                Log.d("Fail", "Fail: ${t.message}")
+            override fun onFailure(call: Call<Array<Double>>, t: Throwable) {
+                Log.d("Fail", "${t.message}")
             }
         })
-
-
+        predictDataCall.enqueue(object : Callback<Array<Double>>{
+            override fun onResponse(call: Call<Array<Double>>, response: Response<Array<Double>>) {
+                if(response.isSuccessful){
+                    val predictData = response.body()
+                    predictDataArray = predictData?.toDoubleArray()
+                    predictDataArray?.forEach { value ->
+                        Log.d("censor", value.toString())
+                    }
+                    if(currentDataArray != null && predictDataArray != null){
+                        navigateToDataFragment(currentDataArray!!, predictDataArray!!)
+                    }
+                }
+                else{
+                    Log.d("Fail", "predictDataCall")
+                }
+            }
+            override fun onFailure(call: Call<Array<Double>>, t: Throwable) {
+                Log.d("Fail", "${t.message}")
+            }
+        })
 
         mBinding = ActivityAppMainBinding.inflate(layoutInflater)
-
         setContentView(mBinding.root)
 
-        //네비게이션 담는 호스트
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.my_nav_host) as NavHostFragment
-
-        //네비게이션 컨트롤러
         val navController = navHostFragment.navController
-
-        //bottom 네비게이션 뷰와 네비게이션 묶기
         NavigationUI.setupWithNavController(mBinding.myBottomNav, navController)
+    }
+
+
+    private fun navigateToDataFragment(currentData: DoubleArray, predictData: DoubleArray) {
+        val bundle = Bundle().apply {
+            putDoubleArray("currentData", currentData)
+            putDoubleArray("predictData", predictData)
+        }
+
+        val navController = findNavController(R.id.my_nav_host)
+        navController.navigate(R.id.dataFragment, bundle)
     }
 }
